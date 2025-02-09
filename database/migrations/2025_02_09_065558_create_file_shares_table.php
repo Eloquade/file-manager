9dcd161432e2547c1b9303e62b2e4f83c1aa0080
Lawrence Perez <lawrence@xperto.ph>
2025-02-09 15:08:30 +0800

    make create tables

--- database/migrations/2025_02_09_065558_create_file_shares_table.php
+++ database/migrations/2025_02_09_065558_create_file_shares_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('file_shares', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->foreignId('file_id')->constrained('files')->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
         });
     }
 
--- database/migrations/2025_02_09_065703_create_starred_files_table.php
+++ database/migrations/2025_02_09_065703_create_starred_files_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('starred_files', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->foreignId('file_id')->constrained('files')->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
         });
     }
 
